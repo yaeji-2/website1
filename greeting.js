1e14cf221ca8eae173924bb7f292cbcabec03c14
yaeji-2 <129691178+yaeji-2@users.noreply.github.com>
2023-04-30 15:25:05 -0500

    Create greeting.js

---
+++ greeting.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('form');
+const greeting = document.querySelector('#greeting');
+
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+	const name = document.querySelector('#name').value;
+	greeting.textContent = `Welcome, ${name}!`;
+	form.reset();
+});
